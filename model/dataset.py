cf42d97f98c291c9497a44211e0c25337cb9cdeb
leoxfox21 <l.eogarciaguerrero22@gmail.com>
2026-05-03 02:27:43 -0400

    Update dataset.py

--- model/dataset.py
+++ model/dataset.py
@@ -3,13 +3,29 @@ import os
 
 FILE = "data/dataset.csv"
 
+
 def save_example(home_stats, away_stats, result_goals, odds, movement):
 
+    home_attack = home_stats["attack"]
+    home_defense = home_stats["defense"]
+    away_attack = away_stats["attack"]
+    away_defense = away_stats["defense"]
+
+    attack_diff = home_attack - away_defense
+    away_diff = away_attack - home_defense
+
+    power = home_attack + away_attack
+    weakness = home_defense + away_defense
+
     row = [
-        home_stats["attack"],
-        home_stats["defense"],
-        away_stats["attack"],
-        away_stats["defense"],
+        home_attack,
+        home_defense,
+        away_attack,
+        away_defense,
+        attack_diff,
+        away_diff,
+        power,
+        weakness,
         odds,
         movement,
         result_goals > 2.5
@@ -26,6 +42,10 @@ def save_example(home_stats, away_stats, result_goals, odds, movement):
                 "home_defense",
                 "away_attack",
                 "away_defense",
+                "attack_diff",
+                "away_diff",
+                "power",
+                "weakness",
                 "odds",
                 "movement",
                 "target"
