98716b7c4ed421ac22a8971ea804188866332b20
leoxfox21 <l.eogarciaguerrero22@gmail.com>
2026-05-03 01:39:43 -0400

    Add predict function for match outcome probabilities
    Implement prediction function using XGBoost model

---
+++ model/predict.py
@@ -0,0 +1,21 @@
+import xgboost as xgb
+import numpy as np
+
+MODEL_FILE = "data/model.json"
+
+model = xgb.XGBClassifier()
+model.load_model(MODEL_FILE)
+
+
+def predict(home_stats, away_stats):
+
+    X = np.array([[
+        home_stats["attack"],
+        home_stats["defense"],
+        away_stats["attack"],
+        away_stats["defense"]
+    ]])
+
+    prob = model.predict_proba(X)[0][1]
+
+    return prob
