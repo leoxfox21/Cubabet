7c4cdf3bcf95de9149e2472ac27f2307d8312ed7
leoxfox21 <l.eogarciaguerrero22@gmail.com>
2026-05-03 01:52:30 -0400

    Update predict.py

--- model/predict.py
+++ model/predict.py
@@ -7,13 +7,15 @@ model = xgb.XGBClassifier()
 model.load_model(MODEL_FILE)
 
 
-def predict(home_stats, away_stats):
+def predict(home_stats, away_stats, odds, movement):
 
     X = np.array([[
         home_stats["attack"],
         home_stats["defense"],
         away_stats["attack"],
-        away_stats["defense"]
+        away_stats["defense"],
+        odds,
+        movement
     ]])
 
     prob = model.predict_proba(X)[0][1]
